9befbb8bec6e45857b4f9611916836c423600995
cabomaldade <soutom@yahoo.com.br>
2018-10-12 23:32:09 -0300

    Initial commit

---
+++ app/src/main/java/com/example/andre/droidcafe/OrderActivity.java
@@ -0,0 +1,31 @@
+package com.example.andre.droidcafe;
+
+//import android.content.Intent;
+//import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+//import android.view.View;
+//import android.widget.TextView;
+
+//import static com.example.andre.droidcafe.MainActivity.EXTRA_MESSAGE;
+
+public class OrderActivity extends AppCompatActivity {
+
+    /*@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        // Get the intent and its data.
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.order_textview);
+        textView.setText(message);
+    }
+
+     não entendi essa parte public void onClick(View view) {
+        Intent intent =
+                new Intent(MainActivity.this, OrderActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+        startActivity(intent);
+    }*/
+}
